15af0803726a6d3b6c60a76c96325238f95eef5f
OdellyaGitFormation <community@odellya.com>
2020-07-27 17:50:45 +0200

    Update.java
    OK

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins Made in O'dellya!");
+        System.out.println("Hello, Jenkins Changement2!");
     }
 }
 
